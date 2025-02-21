6dd303122f17cb84b43640ce8c47fb564ec9b38b
Laxmibhargavisheru <sherulaxmibhargavi@gmail.com>
2025-02-21 19:44:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+//import Header from './Components/Header';
+import Stanjo from './Components/Stanjo';
+
+function App() {
+  return (
+   //<Header/>
+  <Stanjo/>
+  );
+}
+
+export default App;
---
+++ src/Components/Stanjo.jsx
@@ -0,0 +1,263 @@
+import React from 'react'
+import './Stanjo.css'
+import logo from './asserts/Frame.png'
+import log from './asserts/log.png'
+import mail from './asserts/mail.png'
+import facebook from './asserts/facebook.png'
+import insta from './asserts/insta.png'
+import linkedlln from './asserts/linkedlln.png'
+import twitter from './asserts/twitter.png'
+import stanjo from './asserts/stanjo.png'
+import nav from './asserts/nav-bar.png'
+import icon from './asserts/Icon.png'
+import hall from './asserts/hall.jpg'
+import home from './asserts/home.jpg'
+import group from './asserts/group.png'
+import image1 from './asserts/image1.jpg'
+import image2 from './asserts/image2.jpg'
+import image3 from './asserts/image3.jpg'
+import tag1 from './asserts/tag1.png'
+import tag2 from './asserts/tag2.png'
+import tag3 from './asserts/tag3.png'
+import tag4 from './asserts/tag4.png'
+import frame1 from './asserts/frame1.png' 
+import frame2 from './asserts/frame2.png' 
+import frame3 from './asserts/frame3.png' 
+import frame4 from './asserts/frame4.png' 
+import frame5 from './asserts/frame5.png' 
+import frame6 from './asserts/frame6.png' 
+import frame7 from './asserts/frame7.png' 
+import frame8 from './asserts/frame8.png' 
+import frame9 from './asserts/frame9.png' 
+import frame10 from './asserts/frame10.png' 
+import frame11 from './asserts/frame11.png' 
+import stanjoimage from './asserts/stanjo-image.png'
+
+export default function Stanjo() {
+  return (
+    <div class>
+        <div class="container">
+            <div class="top-header">
+                <img src={logo}/>
+                <div class="para">Hyderbad,INDIA</div>
+                <img src={log}/>
+                <div class="para">91+ 6305858219</div>
+                <img src={mail}/>
+                <div class="para">info@Stanjo.in</div>
+            </div>
+            <div class="right-header">
+                <img src={facebook}/>
+                <img src={insta}/>
+                <img src={linkedlln}/>
+                <img src={twitter}/>
+            </div>
+            <div class="top-down">
+            <img class="sta" src={stanjo}/>
+            <div>Home</div>
+            <div >AboutUs|Certifications</div>
+            <div>Products</div>
+            <img class="symbol" src={icon}/>
+            <div>LM79 Reports</div>
+            <div>Shop</div>
+            <div>Contact Us</div>
+            <div>Calculators</div>
+            <img  class="symbol" src={icon}/>
+             <img class="nav-bar" src={nav}/>
+             </div>
+            <div class="inovation">
+            <div class="card">
+             <h1 class="heading">Lighting Your World with Innovation and Efficiency</h1>
+             <p class="paragrap">We Provide High-Performance LED Lights for Every Need</p>
+             <div class="tags">
+             <button class="shop">Shop Now</button>
+             <button class="explore">Explore Products</button>
+            </div>
+            </div>
+            </div>
+            <div>
+            <img  class="hall" src={hall}/>
+        </div>
+        <div class="welcome-card">
+          <div>
+          <h1 class="welcome-head">Welcome to Stanjo-Lighting the way Forward</h1>
+          <p class="welcome-paragraph">At Stanjo, we believe lighting is more than just illumination-it's about enhancing spaces, creating ambiance, and delivering unmatched efficiency. Our mission is to provide high-quality, energy-efficient LED lighting solutions that blend innovation, durability, and style to brighten homes, offices, and outdoor spaces.</p>
+          <button class="collection">Explore Collection</button>
+          </div>
+          <div>
+            <img class="home" src={home}/>
+            <div class="home-card">
+            <h1 class="home-heading">Brighter. Smarter. Built to Last</h1>
+            </div>
+          </div>
+          
+        </div>
+        <div class="background">
+        <h1 class="choose-head">Choose the Perfect </h1>
+        <h1 class="choose-head">Light for Your Space</h1>
+        <p class="choose-para">Explore Our Best-Selling LED Solutions</p>
+        <div class="group-card">
+        <img class="group" src={group}/>
+        <div class="main">
+        <h1 class="group-head">LED Panel Lights</h1>
+        <p class="group-para">Sleek, stylish, and perfect for modern interiors</p>
+        </div>
+        </div>
+      </div>
+      <div class="backgroud-color">
+      <h1 class="choose-head">Not Sure Which Light to</h1>
+      <h1 class="choose-head">Choose? Experience It First!</h1>
+      <p class="choose-para">Still deciding? Our Virtual Lighting Preview lets you see how different lights fit your space before buying.</p>
+      <div class="threeimages"> 
+        <div>
+       <img class="image1" src={image1}/>
+       <div class="img1">
+       <h1>Test warm vs. cool tones</h1>
+       <p>Find the perfect match for your ambiance</p>
+       </div>
+       </div>
+       <div>
+       <img class="image1" src={image2}/>
+       <div class="img2">
+       <h1>Adjust brightness levels</h1>
+       <p>See how different options affect your space</p>
+       </div>
+       </div>
+       <div>
+       <img class="image1" src={image3}/>
+       <div class="img3">
+       <h1>Try it in different rooms</h1>
+       <p>Home, office, outdoor, and more</p>
+       </div>
+       </div>
+      </div>
+      <div class="virtual">
+      <button class="try">Try Virtual Preview</button>
+      </div>
+      </div>
+      <div class="backgroundwhite">
+      <h1 class="choose-head">Why Choose Stanjo?</h1>
+      <p class="choose-para">Premium Quality. Stunning Performance. Unmatched Value</p> 
+      <div class="main-stanjo">
+      <div class="stanjo">
+        <img src={tag1}/>
+        <h1 class="stanjo-head">High-quality LED technology</h1>
+        <p class="stanjo-para">Discover advanced LED lighting that delivers exceptional brightness and durability</p>
+      </div>
+      <div class="stanjo">
+        <img src={tag2}/>
+        <h1 class="stanjo-head">Energy-efficient savings</h1>
+        <p class="stanjo-para">Reduce your power bills without compromising on brightness or performance</p>
+      </div>
+      <div class="stanjo">
+        <img src={tag3}/>
+        <h1 class="stanjo-head">Elegant and modern designs</h1>
+        <p class="stanjo-para">Enhance your space with sleek, stylish lighting solutions that elevate any room</p>
+      </div>
+      <div class="stanjo">
+        <img src={tag4}/>
+        <h1 class="stanjo-head">Trusted by thousands</h1>
+        <p class="stanjo-para">Join thousands of satisfied customers who trust our reliable and long-lasting LED lights</p>
+      </div>
+      <div class="stanjo">
+        <img src={tag1}/>
+        <h1 class="stanjo-head">Longest Warranty</h1>
+        <p class="stanjo-para">Enjoy peace of mind with our industry-leading warranty, ensuring your lights last for years to come</p>
+      </div>
+      <div class="stanjo">
+        <img src={tag2}/>
+        <h1 class="stanjo-head">Low Cost of Ownership</h1>
+        <p class="stanjo-para">Save on energy bills and maintenance costs with our affordable, durable LED lighting solutions</p>
+      </div>
+      <div class="stanjo">
+        <img src={tag3}/>
+        <h1 class="stanjo-head">Bright For Longer</h1>
+        <p class="stanjo-para">Experience consistent, high-quality brightness with LED lights designed to last longer and shine brighter</p>
+      </div>
+      </div>
+  </div>
+  <div class="blue-background">
+    <div class="blue">
+    <h1 class="blue-head">Light Up Your World with Innovation</h1>
+    <p class="blue-para">Explore our premium collection of LED lighting solutions that blend cutting-edge technology, exceptional durability, and energy-efficiency to illuminate every space beautifully. Whether you're looking to enhance your home, office, or outdoor area, our products offer brighter, longer-lasting light while saving you money on energy bills.</p>
+    <div class="now">
+    <button class="download">Download Now</button>
+    </div>
+  </div>
+  </div>
+  <div class="below-background">
+    <h1 class="choose-head">Trusted by Leading Businesses and Industries</h1>
+    <p class="choose-para">Partnering with top brands to deliver reliable, energy-efficient lighting solutions for every space</p>
+     <div class="frames">
+      <img class="frame" src={frame1}/>
+      <img class="frame" src={frame2}/>
+      <img class="frame" src={frame3}/>
+      <img class="frame" src={frame4}/>
+      <img class="frame" src={frame5}/>
+      <img class="frame" src={frame6}/>
+      <img class="frame" src={frame7}/>
+      <img class="frame" src={frame8}/>
+      <img class="frame" src={frame9}/>
+      <img class="frame" src={frame10}/>
+      <img class="frame" src={frame11}/>
+     </div>
+  </div>
+  <div class="back-background">
+    <div>
+    <img class="stan" src={stanjoimage}/>
+    <h1 class="lighting-head">Lighting Your World with Innovation and Efficiency</h1>
+    <div class="ids">
+    <div class="cont">
+    <img class="logo" src={logo}/>
+    <div class="para">Hyderbad,INDIA</div>
+    </div>
+    <div class="cont">
+    <img src={log}/>
+    <div class="para">91+ 6305858219</div>
+    </div>
+    <div class="cont">
+    <img src={mail}/>
+    <div class="para">info@Stanjo.in</div>
+    </div>
+    <div class="footer">
+      <div>
+                <img class="foot"src={facebook}/>
+                </div>
+                <div>
+                <img class="foot" src={insta}/>
+                </div>
+                <div>
+                <img class="foot" src={linkedlln}/>
+                </div>
+                <div>
+                <img class="foot" src={twitter}/>
+          </div>
+    </div>
+    </div>
+    </div>
+    <div class="pro">
+    <div class="products">
+    <h1 class="left-footer">PRODUCTS</h1>
+    <p class="lights">Panel Lights</p>
+    <p class="lights">Flood Lights</p>
+    <p class="lights">Street Lights</p>
+    <p class="lights">Download</p>
+    <p class="lights">Panel Lights</p>
+    <p class="lights">LED Indoor Lights</p>
+    <p class="lights">LED Outdoor Lights</p>
+    <p class="lights">Tubelights & Bulbs</p>
+  </div>
+  <div class="products">
+    <h1 class="left-footer">OTHERS</h1>
+    <p class="lights">About</p>
+    <p class="lights">Products Gallery</p>
+    <p class="lights">Calculators</p>
+    <p class="lights">Contact Us</p>
+    <p class="lights">Privacy Policy</p>
+    <p class="lights">Terms & Conditions</p>
+    </div>
+  </div>
+  </div>
+  </div>
+  </div>
+  )
+}
